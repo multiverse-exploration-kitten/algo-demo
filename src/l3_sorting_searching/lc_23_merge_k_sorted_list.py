5b2101a90a1582f2219fa74b6291ca2fbfecfd40
will_bit <williamc.c999@gmail.com>
2024-07-30 11:11:22 +0800

    update

--- src/l3_sorting_searching/lc_23_merge_k_sorted_list.py
+++ src/l3_sorting_searching/lc_23_merge_k_sorted_list.py
@@ -26,16 +26,16 @@ class Solution:
 
         while l1 and l2:
             if l1.val <= l2.val:
-                point.next = l1
-                l1 = l1.next
+                point.nxt = l1
+                l1 = l1.nxt
             else:
-                point.next = l2
-                l2 = l2.next
-            point = point.next
+                point.nxt = l2
+                l2 = l2.nxt
+            point = point.nxt
 
         if not l1:
             point.next = l2
         else:
             point.next = l1
 
-        return head.next
+        return head.nxt
